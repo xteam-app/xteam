0d9d5123a455cf8ee38ae06237b723d3564a5c11
Alex Ilushyk <ailushyk@gmail.com>
2022-05-03 21:17:27 +0200

    setup dosc-ui as monorepo

--- apps/docs-ui/metro.config.js
+++ apps/docs-ui/metro.config.js
@@ -1,19 +1,33 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
 
-const defaultConfig = getDefaultConfig(__dirname);
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, '../..');
+const projectRoot = __dirname;
+const config = getDefaultConfig(projectRoot);
 
-defaultConfig.resolver.resolverMainFields = [
+config.resolver.resolverMainFields = [
   "sbmodern",
-  ...defaultConfig.resolver.resolverMainFields,
+  ...config.resolver.resolverMainFields,
 ];
 
-defaultConfig.transformer.getTransformOptions = async () => ({
+config.transformer.getTransformOptions = async () => ({
   transform: {
     experimentalImportSupport: false,
     inlineRequires: false,
   },
 });
 
-defaultConfig.watchFolders = [...defaultConfig.watchFolders, "./.ondevice"];
+// 1. Watch all files within the monorepo
+config.watchFolders = [
+  ...config.watchFolders,
+  workspaceRoot,
+  "./.ondevice"
+];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
 
-module.exports = defaultConfig;
+module.exports = config;
