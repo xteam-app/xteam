8b81c25e24e4ae2f1e7ea7133bbd082a83952927
Alex Ilushyk <ailushyk@gmail.com>
2022-05-04 09:45:12 +0200

    connected the core package in the main app

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -4,10 +4,12 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { sayHello } from '@xteam-app/core';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  sayHello()
 
   if (!isLoadingComplete) {
     return null;
