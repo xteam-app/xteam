fcd2b269120da07b51a22543cd177f88e3aa897f
Alex Ilushyk <ailushyk@gmail.com>
2022-05-15 20:57:54 +0200

    add validate script

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -10,6 +10,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     // https://github.com/typescript-eslint/typescript-eslint/tree/main/packages/eslint-plugin
     // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
